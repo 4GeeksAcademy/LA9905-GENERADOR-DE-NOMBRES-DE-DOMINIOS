50b31c22cde0ef3db752fb056deeb1f58751fb74
Alejandro Arraga <alejandroarraga99@gmail.com>
2024-12-14 16:46:39 +0000

    generador de comandos

--- src/app.js
+++ src/app.js
@@ -4,8 +4,17 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["her", "his", "their"];
+  let adj = ["bad", "small", "fast"];
+  let noun = ["book", "bed", "cake"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        console.log(pronoun[p] + adj[a] + noun[n] + ".com");
+      }
+    }
+  }
 };
